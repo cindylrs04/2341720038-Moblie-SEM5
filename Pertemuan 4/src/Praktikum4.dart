a45d2edf0509c52e525c61ac5c7c7f15bc1f28cd
cindylrs04 <larasaticindy231@gmail.com>
2025-09-24 14:27:49 +0700

    update P4

--- Pertemuan 4/src/Praktikum4.dart
+++ Pertemuan 4/src/Praktikum4.dart
@@ -6,10 +6,10 @@ void main() {
   print(list2.length);
 
 // langkah 3
-  var list1 = [1, 2, null];
-  print(list1);
-  var list3 = [0, ...?list1];
-  print(list3.length);
+  // var list1 = [1, 2, null];
+  // print(list1);
+  // var list3 = [0, ...?list1];
+  // print(list3.length);
 
   // Menambahkan NIM
   var nim = [2, 3, 4, 1, 7, 2, 0, 0, 3, 8];
@@ -18,9 +18,9 @@ void main() {
   print(listNim);
 
   //langkah 4
-  var promoActive = false;
-  var nav = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
-  print(nav);
+  // var promoActive = false;
+  // var nav = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
+  // print(nav);
 
   //langkah 5
   var login = 'User'; 

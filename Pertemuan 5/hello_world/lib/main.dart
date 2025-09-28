fa2506671bccf922b4d523cdec6eae61b3fcc4c0
cindylrs04 <larasaticindy231@gmail.com>
2025-09-28 16:44:00 +0700

    add codelab 5

---
+++ Pertemuan 5/hello_world/lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Contoh Date Picker',
+      home: MyHomePage(title: 'Contoh Date Picker'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Variabel state untuk menyimpan tanggal yang dipilih
+  DateTime selectedDate = DateTime.now();
+
+  // Fungsi untuk menampilkan date picker
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate, // default date
+      firstDate: DateTime(2015, 8), // batas awal
+      lastDate: DateTime(2101), // batas akhir
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              // tampilkan tanggal yang dipilih dalam format yyyy-mm-dd
+              "${selectedDate.toLocal()}".split(' ')[0],
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                _selectDate(context);
+                // debug print
+                print(
+                  selectedDate.day +
+                      selectedDate.month +
+                      selectedDate.year,
+                );
+              },
+              child: const Text('Pilih Tanggal'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
